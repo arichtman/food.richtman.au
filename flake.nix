4ed41ac7c7e0e127bf16872de48fe037fba36206
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-08-13 09:19:52 +1000

    fixing dev tooling

--- flake.nix
+++ flake.nix
@@ -1,44 +1,39 @@
 {
-  description = "Hugo flake";
-  nixConfig.bash-prompt = "\[nix-develop\]$ ";
+  description = "Development environment";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    utils = {
+    flake-utils = {
       url = github:numtide/flake-utils;
     };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    # Use p2nix directly for the updates
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
     };
   };
-  outputs = {
-    nixpkgs,
-    utils,
-    self,
-    pre-commit-hooks,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (
-      system: let
+  outputs = { nixpkgs, flake-utils, self, poetry2nix, ... } @ inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
+    (system:
+      let
         pkgs = import nixpkgs {
-          inherit system;
+            inherit system;
         };
-      in {
-        checks = {
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              alejandra.enable = true;
-              markdownlint.enable = true;
-              prettier.enable = true;
-            };
+        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv;
+          poetryEnv = mkPoetryEnv {
+            projectDir = ./.;
           };
-        };
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [hugo];
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
-        };
-      }
-    );
+      in {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              poetry
+              poetryEnv
+              hugo
+            ];
+            shellHook = ''
+              pre-commit install --install-hooks
+            '';
+          };
+    }
+  );
 }
